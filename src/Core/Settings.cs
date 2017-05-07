8095d9d599e2cb4a5f7ce32f43846cd9a2257f59
Jordan Johnson <jordanleej567@gmail.com>
2017-05-07 02:19:39 -0400

    Removed
    Files were related to previous versions.

--- src/Core/Settings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CloudDrip.Core {
-	public class Settings {
-		public static string path {get;set;}
-		public static string url {get;set;}
-		public static string clientId = "bed20744714e9c5962c351efe15840ff";
-	}
-}
